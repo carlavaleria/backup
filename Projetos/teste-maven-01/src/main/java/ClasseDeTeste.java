5d363c1c5d952d283eb31c867c743577bb4e4e2d
Carla Valéria <carla_empresa@outlook.com>
2017-11-13 11:51:21 -0300

    Projetos

---
+++ Projetos/teste-maven-01/src/main/java/ClasseDeTeste.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class ClasseDeTeste {
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("luiz"));
+
+    }
+
+}
